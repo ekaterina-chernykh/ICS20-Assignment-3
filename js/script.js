c07c378f6bafa54528e78b93301c4ff1d5947f6e
ekaterina-chernykh <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-12-10 16:54:39 -0500

    sync

--- js/script.js
+++ js/script.js
@@ -19,11 +19,63 @@ if (navigator.serviceWorker) {
  * This function displays an alert.
  */
 function myButtonClicked() {
-  // input
-  const height = parseFloat(document.getElementById("height").value)
+  let buttonOldFashionDonutChecked = document.getElementById("old-fashioned").checked
+  let buttonChocolateChecked = document.getElementById("chocolate").checked
+  let buttonGlazedChecked = document.getElementById("glazed").checked
+  let button1Checked = document.getElementById("1").checked
+  let button2Checked = document.getElementById("2").checked
+  let button3Checked = document.getElementById("3").checked
+  let button6Checked = document.getElementById("6").checked
+  let button12Checked = document.getElementById("12").checked
 
-  // process
-  const volume = 2
-  // output
-  document.getElementById("volume").innerHTML = "Volume is: " + volume + "mm³"
+  if (buttonOldFashionDonutChecked && button1Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Old Fashion<p> <p>Amount: 1<p> <p>Price:$2.25<p>" 
+  } else if (buttonOldFashionDonutChecked && button2Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Old Fashion<p> <p>Amount: 2<p> <p>Price:$4.75<p>"
+  } else if (buttonOldFashionDonutChecked && button3Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Old Fashion<p> <p>Amount: 3<p> <p>Price:$6.70<p>"
+  } else if (buttonOldFashionDonutChecked && button6Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Old Fashion<p> <p>Amount: 6<p> <p>Price:$10.50<p>"
+  } else if (buttonOldFashionDonutChecked && button12Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Old Fashion<p> <p>Amount: 12<p> <p>Price:$16.50<p>"
+  }
+  
+  if (buttonChocolateChecked && button1Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Chocolate<p> <p>Amount: 1<p> <p>Price:$2.25<p>" 
+  } else if (buttonChocolateChecked && button2Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Chocolate<p> <p>Amount: 2<p> <p>Price:$4.75<p>"
+  } else if (buttonChocolateChecked && button3Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Chocolate<p> <p>Amount: 3<p> <p>Price:$6.70<p>"
+  } else if (buttonChocolateChecked && button6Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Chocolate<p> <p>Amount: 6<p> <p>Price:$10.50<p>"
+  } else if (buttonChocolateChecked && button12Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Chocolate<p> <p>Amount: 12<p> <p>Price:$16.50<p>"
+  }
+
+  if (buttonGlazedChecked && button1Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Glazed<p> <p>Amount: 1<p> <p>Price:$2.25<p>" 
+  } else if (buttonGlazedChecked && button2Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Glazed<p> <p>Amount: 2<p> <p>Price:$4.75<p>"
+  } else if (buttonGlazedChecked && button3Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Glazed<p> <p>Amount: 3<p> <p>Price:$6.70<p>"
+  } else if (buttonGlazedChecked && button6Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Glazed<p> <p>Amount: 6<p> <p>Price:$10.50<p>"
+  } else if (buttonGlazedChecked && button12Checked == true) {
+    document.getElementById("final-order").innerHTML =
+      "<p>Donut: Glazed<p> <p>Amount: 12<p> <p>Price:$16.70<p>"
+  }
 }
